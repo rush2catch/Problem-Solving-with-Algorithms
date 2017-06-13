12bef78faff313df4ae9c6f1a888b4c5926dd542
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-13 19:22:41 +0800

    bubble sort

---
+++ 5_Sorting and Searching/Sorting/BubbleSort.py
@@ -0,0 +1,17 @@
+def bubble_sort(nums):
+	if len(nums) == 0:
+		return None
+
+	for i in range(len(nums)):
+		for j in range(len(nums) - 1):
+			if nums[j] > nums[j + 1]:
+				nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+	return nums
+
+unsorted_list1 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+print(bubble_sort(unsorted_list1))
+unsorted_list2 = []
+print(bubble_sort(unsorted_list2))
+
+
